3ed861fe35f6b52762c7047360d8894a3752e8b9
Christina Chapman <cmchapman75@gmail.com>
2019-12-06 15:33:32 -0600

    added heroku

--- server.js
+++ server.js
@@ -6,47 +6,59 @@ const helmet = require('helmet');
 const movies = require('./movieData.json');
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next){
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
 
-	if(!authToken || authToken.split(' ')[1] !== apiToken){
-		return res.status(401).json({error: 'unathorized request'})
-	}
-	next()
+  if(!authToken || authToken.split(' ')[1] !== apiToken){
+    return res.status(401).json({error: 'unathorized request'});
+  }
+  next();
 });
+
+
 
 app.get('/movie', function handleGetMovie(req,res){
   let results = movies;
-	if(req.query.genre){
-		results= results.filter(movie =>
-			movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())	
-		)
-	}
+  if(req.query.genre){
+    results= results.filter(movie =>
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())	
+    );
+  }
   if(req.query.country){
-		results = results.filter(movie => 
-			movie.country.toLowerCase().includes(req.query.country.toLowerCase())
-		)
-	}
-	if(req.query.avg_vote){
-        results = results.filter(movie => 
-            Number(movie.avg_vote) >= Number(req.query.avg_vote)
-        )
-	}
-    res.json(results);
+    results = results.filter(movie => 
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    );
+  }
+  if(req.query.avg_vote){
+    results = results.filter(movie => 
+      Number(movie.avg_vote) >= Number(req.query.avg_vote)
+    );
+  }
+  res.json(results);
 
 
 });
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error'}};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
 
+const PORT = process.env.PORT || 8000;
 
-
-
-app.listen(8000, () => {
-  console.log('Server started on PORT 8000');
+app.listen(PORT, () => {
+  
 });
